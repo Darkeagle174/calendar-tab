2b57b4ba3e99fd0dd4e6384623d22e95a3fe4330
Dhruv Patel <143174944+Darkeagle174@users.noreply.github.com>
2024-08-26 01:27:35 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        May: [
+            {
+                img: "https://images.pexels.com/photos/5749781/pexels-photo-5749781.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                h1: "Morning Run",
+                copy: "Running around for fun",
+                linkLabel: "Run Daily",
+                linkSrc: ""
+            },
+            {
+                img: "https://images.pexels.com/photos/1037993/pexels-photo-1037993.jpeg",
+                h1: "Morning Run",
+                copy: "Running around for fun",
+                linkLabel: "Run Daily",
+                linkSrc: ""
+            }
+        ]
+    }
+]
+
+export default data;
